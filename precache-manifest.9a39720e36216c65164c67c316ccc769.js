f2c19cfb66e271c6042fc1b1eb92e0c8fedd5df1
Oleksandr Fihun <oleksandr.fihun@globallogic.com>
2020-09-25 16:10:00 +0300

    Updates

---
+++ precache-manifest.9a39720e36216c65164c67c316ccc769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13102152b77afa33bc51a65d2099c24d",
+    "url": "/react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "09a774b96964acdf622f",
+    "url": "/react-hw-03-image-finder/static/css/main.e7b940df.chunk.css"
+  },
+  {
+    "revision": "6ace73024911d5f9586b",
+    "url": "/react-hw-03-image-finder/static/js/2.8ffdd6b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-03-image-finder/static/js/2.8ffdd6b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a774b96964acdf622f",
+    "url": "/react-hw-03-image-finder/static/js/main.9f6dc577.chunk.js"
+  },
+  {
+    "revision": "063253bfbd13644b51dd",
+    "url": "/react-hw-03-image-finder/static/js/runtime-main.f4f19f46.js"
+  }
+]);
